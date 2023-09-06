52a33f638c4212f6b479d44f5c2becaee5a7ca4e
sindhusambasivam <95736922+sindhusambasivam@users.noreply.github.com>
2023-09-06 07:25:45 +0000

    Update jest.test.js

--- jest.test.js
+++ jest.test.js
@@ -3,7 +3,7 @@ const helloworld = require('./helloworld'); // Replace with the actual path to y
 
 describe('GET /hello', () => {
   it('responds with status 200 and "Hello world"', async () => {
-    const response = await request(helloworld).get('/hello'); 
+    const response = await request(helloworld).get('/hello');
     expect(response.status).toBe(200);
     expect(response.text).toBe('Hello world');
   });
