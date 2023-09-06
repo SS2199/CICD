4fd87b9715ac33f2b53fc87c6e071588de6e8262
sindhusambasivam <95736922+sindhusambasivam@users.noreply.github.com>
2023-09-06 07:42:24 +0000

    Update jest.test.js

--- jest.test.js
+++ jest.test.js
@@ -1,9 +1,9 @@
-const request = require('supertest');
-const helloworld = require('./helloworld'); // Replace with the actual path to your Express app
+import request from 'supertest';
+import helloworld from './helloworld'; // Update the import
 
 describe('GET /hello', () => {
   it('responds with status 200 and "Hello world"', async () => {
-    const response = await request(helloworld).get('/hello');
+    const response = await request(helloworld).get('/hello'); // Update the request
     expect(response.status).toBe(200);
     expect(response.text).toBe('Hello world');
   });
