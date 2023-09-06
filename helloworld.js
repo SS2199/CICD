6214a7f51b0d8de3fc8e3ee236abac9791872c81
sindhusambasivam <95736922+sindhusambasivam@users.noreply.github.com>
2023-09-06 06:43:43 +0000

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,12 +1,13 @@
-import express, { request, response } from "express";
+import express from "express";
 
 const app = express();
 const port = 4500;
 
 app.listen(port, () => {
     console.log(`Server running at port: ${port}`);
-  });
+});
 
 app.get("/hello", (req, res) => {
-    console.log("Hello wolrd");
-})
+    console.log("Hello world");
+    res.send("Hello, World!"); // Send a response to the client
+});
