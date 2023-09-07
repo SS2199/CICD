dd0ab888d546848abedb433f8c7e5ad5121e09a8
sd ss <95736922+sindhusambasivam@users.noreply.github.com>
2023-09-07 05:26:42 +0000

    Update jest.test.js

--- jest.test.js
+++ jest.test.js
@@ -1,9 +1,10 @@
 const request = require("supertest");
 const server = require("./helloworld"); // Adjust the path as needed
+const express = require("express"); // Import the express module
 
 describe('GET /hello', () => {
   it('responds with status 200 and "Hello world"', async () => {
-    const response = await request(server).get('/hello'); // Use the server imported from helloworld.js
+    const response = await request(server).get('/hello');
     expect(response.status).toBe(200);
     expect(response.text).toBe('Hello world');
   });
