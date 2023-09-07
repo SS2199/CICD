3a563e1a4db06f68eb4f50fca1ff94b891dccdf0
sd ss <95736922+sindhusambasivam@users.noreply.github.com>
2023-09-07 05:24:27 +0000

    Update

--- helloworld.js
+++ helloworld.js
@@ -1,13 +1,14 @@
-import express from "express";
-
-const app = express();
 const port = 4500;
 
-app.listen(port, () => {
-    console.log(`Server running at port: ${port}`);
+const app = express();
+
+const server = app.listen(port, async () => {
+  console.log(`Server running at port: ${port}`);
 });
 
 app.get("/hello", (req, res) => {
-    console.log("Hello world");
-    res.send("Hello, World!"); // Send a response to the client
+  console.log("Hello world");
+  res.send("Hello world");
 });
+
+module.exports = server; // Export the server for testing
--- jest.test.js
+++ jest.test.js
@@ -1,9 +1,9 @@
-import request from 'supertest';
-import helloworld from './helloworld'; // Update the import
+const request = require("supertest");
+const server = require("./helloworld"); // Adjust the path as needed
 
 describe('GET /hello', () => {
   it('responds with status 200 and "Hello world"', async () => {
-    const response = await request(helloworld).get('/hello'); // Update the request
+    const response = await request(server).get('/hello'); // Use the server imported from helloworld.js
     expect(response.status).toBe(200);
     expect(response.text).toBe('Hello world');
   });
