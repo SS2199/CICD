94fc1bd4646ae8b4aa665641fda7958820990ff2
sindhusambasivam <95736922+sindhusambasivam@users.noreply.github.com>
2023-09-12 11:25:42 +0000

    Create selenium.js

---
+++ selenium.js
@@ -0,0 +1,24 @@
+const driver = new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build();
+
+  try {
+    // Open a webpage
+    await driver.get('http://localhost:8000/');
+
+    // Wait for the title to match the expected title
+    const expectedTitle = 'Hello world';
+    await driver.wait(until.elementLocated(By.xpath("//*[text()='Hello world']")), 5000);// Wait for the title to match
+
+    console.log('Test Passed! Title matches the expected value.');
+  } catch (error) {
+    console.error(`Test Failed! Error: ${error.message}`);
+  } finally {
+    // Close the browser window
+    await driver.quit();
+
+    // Stop the ChromeDriver server
+    chromedriver.stop();
+  }
+})();
