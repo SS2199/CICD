21d81941e279a6cefdcff6a3f0b7045642839a8f
SS2199 <95736922+SS2199@users.noreply.github.com>
2023-09-20 10:19:55 +0000

    Update

--- sample2.js
+++ sample2.js
@@ -1,13 +1,31 @@
-// Octokit.js
-// https://github.com/octokit/core.js#readme
+const { Octokit } = require('@octokit/rest');
+
+// Initialize Octokit with your GitHub Personal Access Token
 const octokit = new Octokit({
-    auth: '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'
-  })
-  
-  await octokit.request('GET /repos/{owner}/{repo}/actions/workflows', {
-    owner: 'SS2199',
-    repo: 'COCD',
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  })
+  auth: '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'
+});
+
+// Define the owner and repository name
+const owner = 'SS2199';
+const repo = 'CICD';
+
+// Make the API request to get workflows
+async function getWorkflows() {
+  try {
+    const response = await octokit.actions.listRepoWorkflows({
+      owner,
+      repo,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+
+    // Handle the response data as needed
+    console.log('Workflows:', response.data);
+  } catch (error) {
+    console.error('Error getting workflows:', error);
+  }
+}
+
+// Call the function to get workflows
+getWorkflows();
